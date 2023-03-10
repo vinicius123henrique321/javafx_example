9039c18b52e65aff64448b6de39c3280592579dc
Vinicius Henrique <112169674+vinicius123henrique321@users.noreply.github.com>
2023-03-10 08:50:50 -0300

    up

---
+++ src/sample/HelloWorld.java
@@ -0,0 +1,59 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class HelloWorld extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        Button btn1 = new Button();
+        Button btn2 = new Button();
+
+        btn.setText("hello");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        btn1.setText("hello");
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        btn2.setText("hello");
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        root.getChildren().add(btn1);
+        root.getChildren().add(btn2);
+        Scene scene = new Scene(root, 500, 400);
+        scene.getStylesheets().add("../src");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ src/sample/scene.java
@@ -0,0 +1,13 @@
+package sample;
+
+import java.util.List;
+
+import javafx.scene.Node;
+
+public class scene {
+
+    public static List<Node> getStylesheets() {
+        return null;
+    }
+
+}
